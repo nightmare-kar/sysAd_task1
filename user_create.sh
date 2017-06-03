53d106426dde85b9587320b1a6499b454861dbff
Your Name <you@example.com>
2017-06-03 22:30:56 +0530

    creating users

---
+++ user_create.sh
@@ -0,0 +1,81 @@
+sudo dscl . -create /Users/user1   // creating user1
+sudo dscl . -create /Users/user1 UserShell /bin/bash
+sudo dscl . -create /Users/user1 RealName "user1"
+sudo dscl . -create /Users/user1 UniqueID "510"
+sudo dscl . -create /Users/user1 PrimaryGroupID 20
+sudo dscl . -create /Users/user1 NFSHomeDirectory /Users/user1
+sudo dscl . -passwd /Users/user1 password
+sudo dscl . -append /Groups/admin GroupMembership user1
+
+
+sudo dscl . -create /Users/user2  //creating user2
+sudo dscl . -create /Users/user2 UserShell /bin/bash
+sudo dscl . -create /Users/user2 RealName "user2"
+sudo dscl . -create /Users/user2 UniqueID "510"
+sudo dscl . -create /Users/user2 PrimaryGroupID 20
+sudo dscl . -create /Users/user2 NFSHomeDirectory /Users/user2
+sudo dscl . -passwd /Users/user2 password
+sudo dscl . -append /Groups/admin GroupMembership user2
+
+sudo dscl . -create /Users/user3
+sudo dscl . -create /Users/user3 UserShell /bin/bash
+sudo dscl . -create /Users/user3 RealName "user3"
+sudo dscl . -create /Users/user3 UniqueID "510"
+sudo dscl . -create /Users/user3 PrimaryGroupID 20
+sudo dscl . -create /Users/user3 NFSHomeDirectory /Users/user3
+sudo dscl . -passwd /Users/user3 password
+sudo dscl . -append /Groups/admin GroupMembership user3
+
+sudo dscl . -create /Users/user4
+sudo dscl . -create /Users/user4 UserShell /bin/bash
+sudo dscl . -create /Users/user4 RealName "user4"
+sudo dscl . -create /Users/user4 UniqueID "510"
+sudo dscl . -create /Users/user4 PrimaryGroupID 20
+sudo dscl . -create /Users/user4 NFSHomeDirectory /Users/user4
+sudo dscl . -passwd /Users/user4 password
+sudo dscl . -append /Groups/admin GroupMembership user4
+
+sudo dscl . -create /Users/user5
+sudo dscl . -create /Users/user5 UserShell /bin/bash
+sudo dscl . -create /Users/user5 RealName "user5"
+sudo dscl . -create /Users/user5 UniqueID "510"
+sudo dscl . -create /Users/user5 PrimaryGroupID 20
+sudo dscl . -create /Users/user5 NFSHomeDirectory /Users/user5
+sudo dscl . -passwd /Users/user5 password
+sudo dscl . -append /Groups/admin GroupMembership user5
+
+sudo dscl . -create /Users/user6
+sudo dscl . -create /Users/user6 UserShell /bin/bash
+sudo dscl . -create /Users/user6 RealName "user6"
+sudo dscl . -create /Users/user6 UniqueID "510"
+sudo dscl . -create /Users/user6 PrimaryGroupID 20
+sudo dscl . -create /Users/user6 NFSHomeDirectory /Users/user6
+sudo dscl . -passwd /Users/user6 password
+sudo dscl . -append /Groups/admin GroupMembership user6
+
+sudo dscl . -create /Users/user7
+sudo dscl . -create /Users/user7 UserShell /bin/bash
+sudo dscl . -create /Users/user7 RealName "user7"
+sudo dscl . -create /Users/user7 UniqueID "510"
+sudo dscl . -create /Users/user7 PrimaryGroupID 20
+sudo dscl . -create /Users/user7 NFSHomeDirectory /Users/user7
+sudo dscl . -passwd /Users/user7 password
+sudo dscl . -append /Groups/admin GroupMembership user7
+
+sudo dscl . -create /Users/user8
+sudo dscl . -create /Users/user8 UserShell /bin/bash
+sudo dscl . -create /Users/user8 RealName "user8"
+sudo dscl . -create /Users/user8 UniqueID "510"
+sudo dscl . -create /Users/user8 PrimaryGroupID 20
+sudo dscl . -create /Users/user8 NFSHomeDirectory /Users/user8
+sudo dscl . -passwd /Users/user8 password
+sudo dscl . -append /Groups/admin GroupMembership user8
+
+sudo dscl . -create /Users/user9
+sudo dscl . -create /Users/user9 UserShell /bin/bash
+sudo dscl . -create /Users/user9 RealName "user9" 
+sudo dscl . -create /Users/user9 UniqueID "510"
+sudo dscl . -create /Users/user9 PrimaryGroupID 20
+sudo dscl . -create /Users/user9 NFSHomeDirectory /Users/user9
+sudo dscl . -passwd /Users/user9 password
+sudo dscl . -append /Groups/admin GroupMembership user9
