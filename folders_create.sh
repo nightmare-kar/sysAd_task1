146043a9d333e38d9835e977b812685f9ae0e31f
Your Name <you@example.com>
2017-06-04 00:02:12 +0530

    creating folders

---
+++ folders_create.sh
@@ -0,0 +1,523 @@
+
+mkdir user1 user2 user3 user4 user5 user6 user7 user8 user9
+//changed users home directory to the above directories in GUI
+cd user1
+mkdir delta
+cd ..
+cd user2
+mkdir delta
+cd ..
+cd user3
+mkdir delta
+cd ..
+cd user4
+mkdir delta
+cd ..
+cd user5
+mkdir delta
+cd ..
+cd user6
+mkdir delta
+cd ..
+cd user7
+mkdir delta
+cd ..
+cd user8
+mkdir delta
+cd ..
+cd user9
+mkdir delta
+cd ..  //creating delta folders for users
+
+
+cd user1
+cd delta
+mkdir folder1 folder2 folder3 folder4 folder5 folder6 folder7 folder8 folder9 folder10
+cd ..
+cd ..
+
+
+cd user2
+cd delta
+mkdir folder1 folder2 folder3 folder4 folder5 folder6 folder7 folder8 folder9 folder10
+cd ..
+cd ..
+
+cd user3
+cd delta
+mkdir folder1 folder2 folder3 folder4 folder5 folder6 folder7 folder8 folder9 folder10
+cd ..
+cd ..
+
+cd user4
+cd delta
+mkdir folder1 folder2 folder3 folder4 folder5 folder6 folder7 folder8 folder9 folder10
+cd ..
+cd ..
+
+cd user5
+cd delta
+mkdir folder1 folder2 folder3 folder4 folder5 folder6 folder7 folder8 folder9 folder10
+cd ..
+cd ..
+
+cd user6
+cd delta
+mkdir folder1 folder2 folder3 folder4 folder5 folder6 folder7 folder8 folder9 folder10
+cd ..
+cd ..
+
+cd user7
+cd delta
+mkdir folder1 folder2 folder3 folder4 folder5 folder6 folder7 folder8 folder9 folder10
+cd ..
+cd ..
+
+cd user8
+cd delta
+mkdir folder1 folder2 folder3 folder4 folder5 folder6 folder7 folder8 folder9 folder10
+cd ..
+cd ..
+
+cd user9
+cd delta
+mkdir folder1 folder2 folder3 folder4 folder5 folder6 folder7 folder8 folder9 folder10
+cd ..
+cd ..
+
+cd user1/delta/folder1    //creating text file for user1
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+
+cd folder2
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder3
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder4
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder5
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder6
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder7
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder8
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder9
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder10
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+cd ..
+cd ..
+
+
+cd user2/delta/folder1    //creating text file for user2
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+
+cd folder2
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder3
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder4
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder5
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder6
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder7
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder8
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder9
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder10
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+cd ..
+cd ..
+
+cd user3/delta/folder1    //creating text file for user3
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+
+cd folder2
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder3
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder4
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder5
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder6
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder7
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder8
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder9
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder10
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+cd ..
+cd ..
+
+
+cd user4/delta/folder1    //creating text file for user4
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+
+cd folder2
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder3
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder4
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder5
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder6
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder7
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder8
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder9
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder10
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+cd ..
+cd ..
+
+
+cd user5/delta/folder1    //creating text file for user5
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+
+cd folder2
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder3
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder4
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder5
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder6
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder7
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder8
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder9
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder10
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+cd ..
+cd ..
+
+
+cd user6/delta/folder1    //creating text file for user6
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+
+cd folder2
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder3
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder4
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder5
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder6
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder7
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder8
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder9
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder10
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+cd ..
+cd ..
+
+cd user7/delta/folder1    //creating text file for user7
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+
+cd folder2
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder3
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder4
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder5
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder6
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder7
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder8
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder9
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder10
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+cd ..
+cd ..
+
+
+cd user8/delta/folder1    //creating text file for user8
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+
+cd folder2
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder3
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder4
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder5
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder6
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder7
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder8
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder9
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder10
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+cd ..
+cd ..
+
+
+cd user9/delta/folder1    //creating text file for user9
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+
+cd folder2
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder3
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder4
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder5
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder6
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder7
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder8
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder9
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder10
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+cd ..
+cd ..
+
+
+cd user10/delta/folder1    //creating text file for user10
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+
+cd folder2
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder3
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder4
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder5
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder6
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder7
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder8
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder9
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+
+cd folder10
+echo '@#$^&^(!(&*)' >u11.txt
+cd ..
+cd ..
+cd ..
